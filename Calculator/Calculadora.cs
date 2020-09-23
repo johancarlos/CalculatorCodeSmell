ab90707e881676aad1084cab5028c0f6584fa26f
Mau Guevara <maugv_77@hotmail.com>
2020-09-23 19:57:15 -0400

    refactoring code smells

---
+++ Calculator/Calculadora.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class Calculadora
+    {
+        public int numero1 { get; set; }
+        public int numero2 { get; set; }
+        public int respuesta { get; set; }
+        public string eleccion { get; set; }
+
+        public Calculadora()
+        {
+            respuesta = 0;
+            Console.WriteLine("====== Calculadora ======");
+            Console.WriteLine("+ sumar  - restar   * multiplicar  / dividir");
+            Console.WriteLine("Elegir Operacion: ");
+            eleccion =Console.ReadLine();
+            Console.WriteLine("Ingrese 2 numeros");
+            Console.WriteLine("numero 1: ");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("numero 2: ");
+            numero2 = int.Parse(Console.ReadLine());
+
+        }
+
+        public void calcular()
+        {
+            switch (eleccion)
+            {
+                case "+":
+                    Console.WriteLine("Suma:");
+                    sumar();
+                    break;
+                case "-":
+                    Console.WriteLine("Resta:");
+                    restar();
+                    break;
+                case "*":
+                    Console.WriteLine("Resta:");
+                    multiplicar();
+                    break;
+                case "/":
+                    Console.WriteLine("Resta:");
+                    dividir();
+                    break;
+            }
+            Console.WriteLine("La respuesta es: {0} {1} {2} = {3}", numero1, eleccion, numero2, respuesta);
+
+        }
+        public void sumar()
+        {
+            respuesta = numero1 + numero2;
+        }
+        public void restar()
+        {
+            respuesta = numero1 - numero2;
+        }
+        public void multiplicar()
+        {
+            respuesta = numero1 * numero2;
+        }
+        public void dividir()
+        {
+            respuesta = numero1 / numero2;
+        }
+
+    }
+}
--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -6,59 +6,17 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Title = "Calculator - CodeSmell";
-            string res = "";
+            string continuar;
+            Calculadora calculadora;
             do
             {
-                int num1 = 0;
-                int num2 = 0;
-                int r = 0;
-
-                Console.WriteLine("====== Calculadora ======");
-                Console.WriteLine("+ sumar");
-                Console.WriteLine("- restar");
-                Console.WriteLine("* multiplicar");
-                Console.WriteLine("- dividir");
-                Console.WriteLine("Elegir Operacion: ");
-
-                res = Console.ReadLine();
-                string eleccion = Convert.ToString(res);
-
-                Console.WriteLine("Ingrese 2 numeros");
-                Console.WriteLine("num1: ");
-                num1 = int.Parse(Console.ReadLine());
-                Console.WriteLine("num2: ");
-                num1 = int.Parse(Console.ReadLine());
-                Console.WriteLine();
-
-                switch (eleccion)
-                {
-                    case "+":
-                        Console.WriteLine("Suma:");
-                        r = num1 + num2;
-                        Console.WriteLine("{0} + {1} = {2}", num1, num2, r);
-                        break;
-                    case "-":
-                        Console.WriteLine("Resta:");
-                        r = num1 - num2;
-                        Console.WriteLine("{0} - {1} = {2}", num1, num2, r);
-                        break;
-                    case "*":
-                        Console.WriteLine("Resta:");
-                        r = num1 * num2;
-                        Console.WriteLine("{0} * {1} = {2}", num1, num2, r);
-                        break;
-                    case "/":
-                        Console.WriteLine("Resta:");
-                        r = num1 / num2;
-                        Console.WriteLine("{0} / {1} = {2}", num1, num2, r);
-                        break;
-                }
+                Console.Clear();
+                calculadora = new Calculadora();
+                calculadora.calcular();
                 Console.Write("Realizar otra operacion? si/no:");
-                res = Console.ReadLine();
+                continuar = Console.ReadLine();
             }
-            while (res == "si");
-            Console.WriteLine("Calculo finalizado");
+            while (continuar == "si");
         }
     }
 }
